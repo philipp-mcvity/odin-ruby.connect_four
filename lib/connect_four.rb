209b4f23da5ea434b71af062469dbf30efdfed06
Philipp Bischoff <bischoff.phil@gmail.com>
2020-05-02 21:18:09 +0200

    initial commit, setup project

---
+++ lib/connect_four.rb
@@ -0,0 +1,4 @@
+# lib/connect_four.rb
+# frozen_string_literal: true
+
+class ConnectFour; end
---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,8 @@
+# spec/connect_four_spec.rb
+# frozen_string_literal: true
+
+require 'connect_four.rb'
+
+describe ConnectFour do
+  subject { ConnectFour.new }
+end
