01dd55b9bd186ca6380c16d65e370a868984b29e
Philipp Bischoff <bischoff.phil@gmail.com>
2020-05-04 22:05:33 +0200

    update spec

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -26,78 +26,50 @@ describe ConnectFour do
 
   describe 'recognizes four consecutives !=nil in an array, expect:' do
     describe '#four_connected?' do
-      it "#{array0 = Array.new(7, nil)} to be false" do
-        expect(subject.four_connected?(array0)).not_to be true
-      end
-      it "#{array1 = Array.new(7, 'a')} to be true" do
-        expect(subject.four_connected?(array1)).to be true
-      end
-      it "#{array2 = Array.new(7, 'b')} to be true" do
-        expect(subject.four_connected?(array2)).to be true
-      end
-      it "#{array3 = [*%w[a a a], nil, *%w[b b b]]} to be false" do
-        expect(subject.four_connected?(array3)).not_to be true
-      end
-      it "#{array4 = [*%w[b a b a], nil, *%w[b a]]} to be false" do
-        expect(subject.four_connected?(array4)).not_to be true
-      end
-      it "#{array5 = %w[b a b a b a b a]} to be false" do
-        expect(subject.four_connected?(array5)).not_to be true
-      end
-      it "#{array6 = [*%w[a a a], nil, 'a']} to be false" do
-        expect(subject.four_connected?(array6)).not_to be true
-      end
-      it "#{array7 = [*%w[a a a], nil, *%w[a a]]} to be false" do
-        expect(subject.four_connected?(array7)).not_to be true
-      end
-      it "#{array8 = [*%w[a a a], nil, *%w[a a a]]} to be false" do
-        expect(subject.four_connected?(array8)).not_to be true
-      end
-      it "#{array9 = %w[a a a b a a a b]} to be false" do
-        expect(subject.four_connected?(array9)).not_to be true
-      end
-      it "#{array10 = %w[a a a a b a a b]} to be true" do
-        expect(subject.four_connected?(array10)).to be true
-      end
-      it "#{array11 = %w[b a a a a b a]} to be true" do
-        expect(subject.four_connected?(array11)).to be true
-      end
-      it "#{array12 = %w[a a a b a a a a]} to be true" do
-        expect(subject.four_connected?(array12)).to be true
-      end
-      it "#{array13 = %w[a a b a a a a]} to be true" do
-        expect(subject.four_connected?(array13)).to be true
-      end
-      it "#{array14 = [nil, nil, nil, nil, *%w[a a a a]]} to be true" do
-        expect(subject.four_connected?(array14)).to be true
-      end
-      it "#{array15 = [nil, 'a', nil, *%w[a a a]]} to be false" do
-        expect(subject.four_connected?(array15)).not_to be true
-      end
-      it "#{array16 = [nil, 'b', nil, *%w[b b b]]} to be false" do
-        expect(subject.four_connected?(array16)).not_to be true
-      end
-      it "#{array17 = [*%w[b b b], nil, *%w[a a a]]} to be false" do
-        expect(subject.four_connected?(array17)).not_to be true
-      end
-      it "#{array18 = [*%w[b b b], nil, *%w[b b b b]]} to be true'" do
-        expect(subject.four_connected?(array18)).to be true
-      end
-      it "#{array19 = [*%w[b b b b], nil, *%w[a a a]]} to be true" do
-        expect(subject.four_connected?(array19)).to be true
-      end
-      it "#{array20 = [nil, 'b', nil, *%w[b b b b]]} to be true" do
-        expect(subject.four_connected?(array20)).to be true
-      end
-      it "#{array21 = %w[a a b b b b a]} to be true" do
-        expect(subject.four_connected?(array21)).to be true
-      end
-      it "#{array22 = [nil, nil, nil, nil, *%w[b b b b]]} to be true" do
-        expect(subject.four_connected?(array22)).to be true
-      end
-      it "#{array23 = [nil, nil, nil, nil, *%w[b b b]]} to be false" do
-        expect(subject.four_connected?(array23)).not_to be true
-      end
+      array0 = Array.new(7, nil)
+      array1 = Array.new(7, 'a')
+      array3 = [*%w[a a a], nil, *%w[b b b]]
+      array4 = [*%w[b a b a], nil, *%w[b a]]
+      array5 = %w[b a b a b a b a]
+      array7 = [*%w[a a a], nil, *%w[a a]]
+      array8 = [*%w[a a a], nil, *%w[a a a]]
+      array2 = [*%w[a a a], nil, *%w[a a a a]]
+      array9 = %w[a a a b a a a b]
+      array10 = %w[a a a a b a a b]
+      array11 = %w[b a a a a b a]
+      array12 = %w[a a a b a a a a]
+      array13 = %w[a a b a a a a]
+      array14 = [nil, nil, nil, nil, *%w[a a a a]]
+      array15 = [nil, 'a', nil, *%w[a a a]]
+      array16 = [nil, 'b', nil, *%w[b b b]]
+      array17 = [*%w[b b b], nil, *%w[a a a]]
+      array18 = [*%w[b b b], nil, *%w[b b b b]]
+      array19 = [*%w[b b b b], nil, *%w[a a a]]
+      array20 = [nil, 'b', nil, *%w[b b b b]]
+      array22 = [nil, nil, nil, nil, *%w[b b b b]]
+      array23 = [nil, nil, nil, nil, *%w[b b b]]
+      it { expect(subject.four_connected?(array0)).not_to be true }
+      it { expect(subject.four_connected?(array1)).to be true }
+      it { expect(subject.four_connected?(array3)).not_to be true }
+      it { expect(subject.four_connected?(array4)).not_to be true }
+      it { expect(subject.four_connected?(array5)).not_to be true }
+      it { expect(subject.four_connected?(array7)).not_to be true }
+      it { expect(subject.four_connected?(array8)).not_to be true }
+      it { expect(subject.four_connected?(array2)).to be true }
+      it { expect(subject.four_connected?(array9)).not_to be true }
+      it { expect(subject.four_connected?(array10)).to be true }
+      it { expect(subject.four_connected?(array11)).to be true }
+      it { expect(subject.four_connected?(array12)).to be true }
+      it { expect(subject.four_connected?(array13)).to be true }
+      it { expect(subject.four_connected?(array14)).to be true }
+      it { expect(subject.four_connected?(array15)).not_to be true }
+      it { expect(subject.four_connected?(array16)).not_to be true }
+      it { expect(subject.four_connected?(array17)).not_to be true }
+      it { expect(subject.four_connected?(array18)).to be true }
+      it { expect(subject.four_connected?(array19)).to be true }
+      it { expect(subject.four_connected?(array20)).to be true }
+      it { expect(subject.four_connected?(array22)).to be true }
+      it { expect(subject.four_connected?(array23)).not_to be true }
     end
   end
 
